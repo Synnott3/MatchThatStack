d2aff30ffdf5d9ef539a86fa0fcc10b10715a7a4
Synnott3 <adamsynnott32@gmail.com>
2022-11-17 01:21:07 +0000

    adding cardarry of six cards, then douvle to have 12 to pcik from

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,63 @@
-console.log("Connection!");
+const cardArray = [
+    {
+        name: 'beaver',
+        img: 'assets/images/beaver.png',
+    },
+
+    {
+        name: 'donkey',
+        img: 'assets/images/donkey.png',
+    },
+    
+    {
+        name: 'duck',
+        img: 'assets/images/duck.png',
+    },
+
+    {
+        name: 'koala',
+        img: 'assets/images/koala.png',
+    },
+
+    {
+        name: 'parrot',
+        img: 'assets/images/parrot.png',
+    },
+
+    {
+        name: 'roo',
+        img: 'assets/images/roo.png',
+    },
+
+    {
+        name: 'beaver',
+        img: 'assets/images/beaver.png',
+    },
+
+    {
+        name: 'donkey',
+        img: 'assets/images/donkey.png',
+    },
+    
+    {
+        name: 'duck',
+        img: 'assets/images/duck.png',
+    },
+
+    {
+        name: 'koala',
+        img: 'assets/images/koala.png',
+    },
+
+    {
+        name: 'parrot',
+        img: 'assets/images/parrot.png',
+    },
+
+    {
+        name: 'roo',
+        img: 'assets/images/roo.png',
+    }
+]
+
+console.log(cardArray);
