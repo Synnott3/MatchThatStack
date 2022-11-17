54f5f635c29f1da9f68cf957ec7c6d4097690b5d
Synnott3 <adamsynnott32@gmail.com>
2022-11-17 02:24:12 +0000

    adding more the flipCard array, pushing items into t he cardPicked array, test in console logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,11 +59,14 @@ const cardArray = [
         img: 'assets/images/roo.png',
     }
 ]
-
-//This shuffles the array randomly by seeing if the value is smaller than 0.5 or larger than 0.5
+/**
+ * /This shuffles the array randomly by seeing if the value is smaller than 0.5 or larger than 0.5
+ * The cardPicked array is a const as we are only pushing things into it
+ */
 cardArray.sort(() => 0.5 - Math.random())
 
 const gameareaDisplay = document.querySelector('.game-area')
+const cardPicked = []
 
 function createBoard () {
     for (let i = 0; i < cardArray.length; i++) {
@@ -77,7 +80,14 @@ function createBoard () {
 
 createBoard () 
 
+/**
+ * This function will flip the cards
+ * the "this" keyword is allowing interaction with the elements we click 
+ * and storing the cardID
+ */
 function flipCard () {
     const cardId = this.getAttribute('date-type')
+    cardPicked.push(cardArray[cardId].name)
     console.log('clicked', cardId)
+    console.log(cardPicked)
 }
