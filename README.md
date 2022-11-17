cedcaa7af0762b30f5b0db1e1db8fb9e3bbf2860
Synnott3 <adamsynnott32@gmail.com>
2022-11-17 09:48:33 +0000

    Adding more information to the README file as well as adding images for it

--- README.md
+++ README.md
@@ -18,3 +18,53 @@ I designned this game to be simple as it was made for my nephews - but it has ro
 
 I did have a logo at the top of this page but it came into conflic of the fact I was using the SCR tag to cause my images to flip so had to be removed, but it helped bring the website back down to its base idea - which was to make an website that would have a fun game that could be easily updated and chagned for my  Nephews. 
 
+![Simple welcoming back - all the information easy to understand](assets/images/ReadmeImg/Forkids.PNG)
+
+### Easy to undertand messages and pop ups
+
+As you can see this game is designed to be pick up and play have some fun make a mistake - work on your memory and don't be discouraged if you get something wrong. 
+
+![Getting it correct](assets/images/ReadmeImg/SimpleM.jpg)
+
+![Making a mistake](assets/images/ReadmeImg/SimpleM2.jpg)
+
+#### The Game Area
+
+The game area is set up in a grid system of 12 cards, 6 of which are the same, and any amount can be addede into the future at any time within the code just be doulbing the grid amount - this was done so that it could be easily edited for whoever is uing it own personal intersets. 
+
+#### The interactive part of the game itself
+
+The game works by pressing the squares and they flip over to relieve an animal underneath - and they allows you to pick a second one and if they match they both disapear and you get a point, if they don't they both flip back over. This is where the memory part comes into play, as they are only random position once loading, but stay in place till the end of the game. So even if you're wrong, you are one step closing to finishing the game.
+
+## Testing 
+
+- The website and functionalilty of the game was tested on Chrome, Firefox, Microsfot Edge and many mobile phones - ranging from Iphones to Andriods.
+
+- Test that each of the squares can be activated and do react on  every device and broswer - as well making sure all message windows pop up and you can complete the game and the hey you won message pops up. 
+
+- Test that every image does dispear when matched.
+
+- Test that every image flips back over when incorrect.
+
+## Bugs 
+
+- The only bug found while working on this project is using  SRC in the setAttribute on elements in my JavaScript   caused my logo to be replaced by a card at random times - hence why i had to remove the logo from top of the page. 
+
+- Bug found while building, looping problems when not setting the correct amount in the Arrays, 
+
+- Fitting correctly once I started to add HTML and CSS as I build  out the JavsScript first.
+
+- All buys now fixed.
+
+## Validator Testing
+
+### Lighthouse - Passed
+
+![lighthouse validation screenshot](assets/images/ReadmeImg/lighthouse.PNG)
+
+### HTML - Passed 
+
+![HTML validation screenshot](assets/images/ReadmeImg/HTMLpassed.PNG)
+
+### CSS - Passed
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -61,62 +61,62 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'roo',
             img: 'assets/images/Cards/roo.png',
         }
-    ]
+    ];
     /**
      * /This shuffles the array randomly by seeing if the value is smaller than 0.5 or larger than 0.5
      * The cardPicked array is a const as we are only pushing things into it
      */
-    cardArray.sort(() => 0.5 - Math.random())
+    cardArray.sort(() => 0.5 - Math.random());
 
     const gameareaDisplay = document.querySelector('.game-area')
     const scoreDisplay = document.querySelector('#score')
-    let cardPicked = []
-    let cardPickedId = []
-    let cardsWon = []
+    let cardPicked = [];
+    let cardPickedId = [];
+    let cardsWon = [];
 //Creates the game board
     function createBoard () {
         for (let i = 0; i < cardArray.length; i++) {
-            const card = document.createElement('img')
-            card.setAttribute('src', 'assets/images/Cards/blank.png')
-            card.setAttribute('data-type', i)
-            card.addEventListener('click', flipCard)
-            gameareaDisplay.append(card)
+            const card = document.createElement('img');
+            card.setAttribute('src', 'assets/images/Cards/blank.png');
+            card.setAttribute('data-type', i);
+            card.addEventListener('click', flipCard);
+            gameareaDisplay.append(card);
         }
     }
 
     
 //The check for matches function
     function checkMatch() {
-        const cards = document.querySelectorAll('img')
-        const optionOneId = cardPickedId[0]
-        const optionTwoId = cardPickedId[1]
+        const cards = document.querySelectorAll('img');
+        const optionOneId = cardPickedId[0];
+        const optionTwoId = cardPickedId[1];
 
         if (optionOneId == optionTwoId) {
-            cards[optionOneId].setAttribute('src','assets/images/Cards/blank.png')
-            cards[optionTwoId].setAttribute('src','assets/images/Cards/blank.png')
-            alert('That is the same animal')
+            cards[optionOneId].setAttribute('src','assets/images/Cards/blank.png');
+            cards[optionTwoId].setAttribute('src','assets/images/Cards/blank.png');
+            alert('That is the same animal');
         }
 
  //Will replace with a white card if there is match!       
         else if (cardPicked[0]  == cardPicked[1]) {
-            alert('It is a match!')
-            cards[optionOneId].setAttribute('src', 'assets/images/Cards//white.png')
-            cards[optionTwoId].setAttribute('src', 'assets/images/Cards//white.png')
-            cards[optionOneId].removeEventListener('click', flipCard)
-            cards[optionTwoId].removeEventListener('click', flipCard)
-            cardsWon.push(cardPicked)
+            alert('It is a match!');
+            cards[optionOneId].setAttribute('src', 'assets/images/Cards//white.png');
+            cards[optionTwoId].setAttribute('src', 'assets/images/Cards//white.png');
+            cards[optionOneId].removeEventListener('click', flipCard);
+            cards[optionTwoId].removeEventListener('click', flipCard);
+            cardsWon.push(cardPicked);
         } else {
-            cards[optionOneId].setAttribute('src', 'assets/images/Cards/blank.png')
-            cards[optionTwoId].setAttribute('src', 'assets/images/Cards/blank.png')
-            alert('That was not it champ try again')
+            cards[optionOneId].setAttribute('src', 'assets/images/Cards/blank.png');
+            cards[optionTwoId].setAttribute('src', 'assets/images/Cards/blank.png');
+            alert('That was not it champ try again');
         }
         
 //Winning text!     
-        cardPicked = []
-        cardPickedId = []
+        cardPicked = [];
+        cardPickedId = [];
         scoreDisplay.textContent = cardsWon.length
             if (cardsWon.length === cardArray.length/2) {
-                scoreDisplay.textContent = "Hey you won!"
+                scoreDisplay.textContent = "Hey you won!";
         }
     }
 
@@ -130,14 +130,14 @@ document.addEventListener('DOMContentLoaded', () => {
      
 //Flips the iamges based on timer as well as src tag
     function flipCard () {
-        let cardId = this.getAttribute('data-type')
-        cardPicked.push(cardArray[cardId].name)
-        cardPickedId.push(cardId)
-        this.setAttribute('src', cardArray[cardId].img)
+        let cardId = this.getAttribute('data-type');
+        cardPicked.push(cardArray[cardId].name);
+        cardPickedId.push(cardId);
+        this.setAttribute('src', cardArray[cardId].img);
         if (cardPicked.length ===2) {
-            setTimeout(checkMatch, 300)
+            setTimeout(checkMatch, 300);
         }
     }
 
-    createBoard () 
-})
+    createBoard ();
+});
